2b1cf368e9d6e8f1364f72c415c551e46083423f
Yesh Jain <135646021+Yesh-jain@users.noreply.github.com>
2024-10-07 12:37:15 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -11,7 +11,7 @@ def decode_message(s: str, p: str) -> bool:
     # Fill the dp table
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if p[j - 1] == s[i - 1] or p[j - 1] == '?':
+            if p[j - 1] == s[i - 1] or p[j - 1] == '?' :
                 dp[i][j] = dp[i - 1][j - 1]
             elif p[j - 1] == '*':
                 dp[i][j] = dp[i][j - 1] or dp[i - 1][j]  # * matches empty or any sequence of chars
